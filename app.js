32940fca3fd69f2c8b72eb15e86907eb7f8ce61f
Weiyu Tang <twyla9876@outlook.com>
2021-10-14 17:59:09 +0100

    cookies

--- app.js
+++ app.js
@@ -33,41 +33,58 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('12345-67890-09876-54321'));
+// we are passing a secret key to use the signed cookies
 
+// user basic authentication
 function auth(req,res,next) {
-  console.log(req.headers);
+  console.log(req.signedCookies);
 
-  var authHeader = req.headers.authorization;
+  if (!req.signedCookies.user) { // if the incoming request does not include the user field in the signed
+    //cookies, then the user has not been authorized yet
+    var authHeader = req.headers.authorization;
 
-  if (!authHeader) { // this means our cliend did not include the username and the password into
-    // the authentication header
-    var err = new Error('You are not authenticated!');
-
-    res.setHeader('WWW-Authenticate', 'Basic');
-    err.status = 401;
-    next(err);
-  }
-
-  var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
-  // the result of the above code will be an array containing a username and password
-  // buffer enables you to split a string value
-  // will split the string into an array, and the first element of the array will be basic
-  // the second element of array will be base64 encoded string
-  // so [1] means we are only looking for the second element in the array
-
-  var username = auth[0];
-  var password = auth[1];
-
-  if (username === 'admin' && password === 'password') {
-    next();
+    if (!authHeader) { // this means our cliend did not include the username and the password into
+      // the authentication header
+      var err = new Error('You are not authenticated!');
+  
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      next(err);
+    }
+  
+    var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+    // the result of the above code will be an array containing a username and password
+    // buffer enables you to split a string value
+    // will split the string into an array, and the first element of the array will be basic
+    // the second element of array will be base64 encoded string
+    // so [1] means we are only looking for the second element in the array
+  
+    var username = auth[0];
+    var password = auth[1];
+  
+    if (username === 'admin' && password === 'password') {
+      res.cookie('user', 'admin', {signed: true}) // we set up the cookie, the user is admin
+      // all the outgoing requests will automatically include this cookie
+      next();
+    }
+    else {
+      var err = new Error('You are not authenticated!');
+  
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      next(err);
+    }
   }
   else {
-    var err = new Error('You are not authenticated!');
-
-    res.setHeader('WWW-Authenticate', 'Basic');
-    err.status = 401;
-    next(err);
+    if (req.signedCookies.user === 'admin') {
+      next();
+    }
+    else {
+      var err = new Error('You are not authenticated!');
+      err.status = 401;
+      next(err);
+    }
   }
 }
 
